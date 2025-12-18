3d160b82f5312c2bb00bb927e7c6cf570f869868
irem <iremtopcu69@gmail.com>
2025-12-18 14:55:57 +0300

    Initial commit

--- src/TestFruit.java
+++ src/TestFruit.java
@@ -1,8 +1,7 @@
 public class TestFruit {
     public static void main(String[] args){
-    Citrus c=new Citrus("lemon",50.5,20.5);
+
     Orange o=new Orange("orange",10.5,20.8,4 );
-    c.describe();
     o.describe();
 
 
